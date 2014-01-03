6732dd7a95e5ccbaece38bf51f2635e5179b9550
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2014-01-03 06:31:18 +0100

    - added siglevel options for pacman.conf - kde style theme fix

--- wm-dvd-iso/root-image/etc/xdg/awesome/rc.lua
+++ wm-dvd-iso/root-image/etc/xdg/awesome/rc.lua
@@ -38,7 +38,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/blackarch/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
