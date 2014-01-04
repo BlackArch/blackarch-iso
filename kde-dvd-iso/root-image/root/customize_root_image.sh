4779dbf75b18c00f9ec728ba587b4d516f0f2ec0
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2014-01-04 06:19:32 +0100

    .

--- kde-dvd-iso/root-image/root/customize_root_image.sh
+++ kde-dvd-iso/root-image/root/customize_root_image.sh
@@ -10,8 +10,6 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 
-#useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh arch
-
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
 
@@ -19,6 +17,7 @@ sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
 systemctl enable pacman-init.service choose-mirror.service
+
 
 ### blackarch related ###
 
