b2dce76f53e618e3cf1520b44165658c81b50658
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2014-01-08 22:26:11 +0100

    added blackarch-install text file to /root

--- wm-dvd-iso/root-image/root/customize_root_image.sh
+++ wm-dvd-iso/root-image/root/customize_root_image.sh
@@ -43,11 +43,4 @@ su -c 'pacman -Syy --noconfirm' root
 su -c 'chmod 755 /usr/local/bin/blackarch-dwm'
 
 # blackarch-install (dev version)
-su -c 'cd /root; git clone https://github.com/BlackArch/blackarch-install-scripts' root
-su -c 'mkdir /usr/share/blackarch-install-scripts' root
-su -c 'cd /root;cd blackarch-install-scripts; cp -R blackarch-install chroot-install grub /usr/share/blackarch-install-scripts/' root
-su -c 'rm -rf /usr/bin/blackarch-install; ln -s /usr/share/blackarch-install-scripts/blackarch-install /usr/bin/' root
-su -c 'cd root; cd blackarch-install-scripts; cp blackarch-install.txt /root' root
-su -c 'cd /root;rm -rf blackarch-install-scripts' root
-su -c 'rm -rf /root/install.txt' root
 su -c 'cp /usr/share/doc/blackarch-install-scripts/blackarch-install.txt /root/' root
