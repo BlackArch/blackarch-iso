0bd42111042d4f7cf1928fd7af346e6796c54c16
Pedro Freitas <pedrosffreitas@gmail.com>
2018-05-15 19:31:17 -0300

    customize: changes for the new release.
    Signed-off-by: Pedro Freitas <pedrosffreitas@gmail.com>

--- live-iso/airootfs/root/customize_airootfs.sh
+++ live-iso/airootfs/root/customize_airootfs.sh
@@ -1,81 +1,90 @@
 #!/bin/bash
 
-#set -e -u
+# exit on error and undefined variables
+set -eu
 
+# set locale
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
+# set timezone
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-cp -aT /etc/skel/ /root/
+# enabling all mirros
+sed -i "s|#Server|Server|g" /etc/pacman.d/mirrorlist
 
-sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+# storing the system journal in RAM
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
+# default releng configuration
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service lxdm.service dbus \
-  vmware-vmblock-fuse.service vmtoolsd.service
+# enable useful services and display manager
+enabled_services=('choose-mirror.service' 'lxdm.service' 'dbus'
+                  'vmware-vmblock-fuse.service' 'vmtoolsd.service')
+systemctl enable ${enabled_services[@]}
 systemctl set-default graphical.target
 
 # create the user directory for live session
-if [ ! -d /root ]
-then
-	mkdir /root && chmod 700 /root && chown -R root:root /root
+if [ ! -d /root ]; then
+    mkdir /root
+    chmod 700 /root && chown -R root:root /root
 fi
 
-# copy files over to home
-su -c "cp -r /etc/skel/. /root/." root
-
-# setup repository, add pacman.conf entry, sync databases
-su -c 'curl -s https://blackarch.org/strap.sh | sh' root
-
-# sys updates, cleanups, etc.
-su -c 'pacman -Syyu --noconfirm' root
-#su -c "pacman -Rscn \$(pacman -Qtdq)"
-su -c 'pacman-optimize' root
-su -c 'updatedb' root
-su -c 'pacman-db-upgrade' root
-su -c 'pkgfile -u' root
-su -c 'pacman -Syy' root
-su -c 'pacman -Scc --noconfirm' root
-su -c 'sync' root
-
-# fix wrong permissions for blackarch-dwm
-su -c 'chmod 755 /usr/bin/blackarch-dwm'
-
-# default shell
-su -c 'usermod -s /bin/bash root' root
-
 # disable pc speaker beep
-su -c 'echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf' root
-
-# download and install exploits
-su -c 'sploitctl -f 0 -v' root
-
-# temporary fixes for ruby based tools
-su -c 'cd /usr/share/arachni/ && bundle config build.nokogiri --use-system-libraries && bundle install --path vendor/bundle' root
-su -c 'cd /usr/share/smbexec/ && bundle config build.nokogiri --use-system-libraries && bundle install --path vendor/bundle' root
-su -c 'cd /usr/share/beef/ && bundle config build.nokogiri --use-system-libraries && bundle install --path vendor/bundle' root
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
 # disable network stuff
 rm /etc/udev/rules.d/81-dhcpcd.rules
 systemctl disable dhcpcd sshd rpcbind.service
 
-# remove not needed .desktop entries
-su -c 'rm -rf /usr/share/xsessions/openbox-kde.desktop' root
-su -c 'rm -rf /usr/share/xsessions/i3-with-shmlog.desktop' root
+# remove special (not needed) files
+rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
+rm /root/{.automated_script.sh,.zlogin}
 
-# remove special (not needed) scripts
-su -c 'rm /etc/systemd/system/getty@tty1.service.d/autologin.conf' root
-su -c 'rm /root/{.automated_script.sh,.zlogin}' root
-su -c 'rm /etc/mkinitcpio-archiso.conf' root
-su -c 'rm -r /etc/initcpio' root
+# setting root password
+echo "root:blackarch" | chpasswd
+
+# copy files over to home
+cp -r /etc/skel/. /root/.
+
+# setup repository, add pacman.conf entry, sync databases
+pacman -Syy --noconfirm
+pacman-optimize
+pacman-db-upgrade
+pacman-key --init
+# install BlackArch repository with default mirror (that's why the sed)
+curl -s https://blackarch.org/strap.sh | \
+    sed "s|get_mirror$|#get_mirror|1" | sh
+pacman-key --populate blackarch archlinux
+
+# font configuration
+ln -sfv /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+ln -sfv /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+ln -sfv /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+
+# fix wrong permissions for blackarch-dwm
+chmod 755 /usr/bin/blackarch-dwm
+
+# default shell
+chsh -s /bin/bash
+
+# download and install exploits
+sploitctl -f 0 -v
+
+# temporary fixes for ruby based tools
+cd /usr/share/arachni/ && bundle config build.nokogiri --use-system-libraries && bundle install --path vendor/bundle
+cd /usr/share/smbexec/ && bundle config build.nokogiri --use-system-libraries && bundle install --path vendor/bundle
+cd /usr/share/beef/ && bundle config build.nokogiri --use-system-libraries && bundle install --path vendor/bundle
+
+# remove not needed .desktop entries
+rm -rf /usr/share/xsessions/openbox-kde.desktop
+rm -rf /usr/share/xsessions/i3-with-shmlog.desktop
 
 # add install.txt file
-su -c 'echo "type blackarch-install and follow the instructions" > /root/install.txt'
+echo "type blackarch-install and follow the instructions" > /root/install.txt
 
 # GDK Pixbuf
 gdk-pixbuf-query-loaders --update-cache
