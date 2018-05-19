dc807eef4bed0e2ab203bcdc5b77e661e4813d09
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2018-05-19 02:16:20 +0200

    remove test files

--- live-iso-test/airootfs/etc/skel/scripts/sysclean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pacman -Rscn $(pacman -Qtdq)
-pkgfile -u
-pacman-optimize
-updatedb
-pacman-db-upgrade
-yes | pacman -Scc
-sync
--- live-iso-test/airootfs/etc/wmii/python/pyxp/__init__.py
+++
@@ -1,7 +0,0 @@
-from pyxp.client import Client
-from pyxp.dial import dial
-from pyxp.types import Qid, Stat
-
-VERSION = '9P2000'
-
-# vim:se sts=4 sw=4 et:
