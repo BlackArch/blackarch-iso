223d62104ad7ab45ac7b1d72bcae7565488b3880
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2019-12-27 01:02:38 +0100

    add files.

---
+++ live-iso/airootfs/etc/skel/.vim/bundle/clang_complete/plugin/snippets/clang_complete.py
@@ -0,0 +1,48 @@
+import re
+import vim
+
+def snippetsInit():
+  python_cmd = vim.eval('s:py_cmd')
+  vim.command("noremap <silent> <buffer> <tab> :{} updateSnips()<CR>".format(python_cmd))
+  vim.command("snoremap <silent> <buffer> <tab> <ESC>:{} updateSnips()<CR>".format(python_cmd))
+  if int(vim.eval("g:clang_conceal_snippets")) == 1:
+    vim.command("syntax match placeHolder /\$`[^`]*`/ contains=placeHolderMark")
+    vim.command("syntax match placeHolderMark contained /\$`/ conceal")
+    vim.command("syntax match placeHolderMark contained /`/ conceal")
+
+# The two following function are performance sensitive, do _nothing_
+# more that the strict necessary.
+
+def snippetsFormatPlaceHolder(word):
+  return "$`%s`" % word
+
+def snippetsAddSnippet(fullname, word, abbr):
+  return word
+
+r = re.compile('\$`[^`]*`')
+
+def snippetsTrigger():
+  if r.search(vim.current.line) is None:
+    return
+  vim.command('call feedkeys("\<esc>^\<tab>")')
+
+def snippetsReset():
+  pass
+
+def updateSnips():
+  line = vim.current.line
+  row, col = vim.current.window.cursor
+
+  result = r.search(line, col)
+  if result is None:
+    result = r.search(line)
+    if result is None:
+      vim.command('call feedkeys("\<c-i>", "n")')
+      return
+
+  start, end = result.span()
+  vim.current.window.cursor = row, start
+  isInclusive = vim.eval("&selection") == "inclusive"
+  vim.command('call feedkeys("\<ESC>v%dl\<C-G>", "n")' % (end - start - isInclusive))
+
+# vim: set ts=2 sts=2 sw=2 expandtab :
