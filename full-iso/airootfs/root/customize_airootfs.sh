8a95064d64a0348b65b5e9dfcd164f8c2cabbf44
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2021-08-13 13:58:54 +0200

    fix fluxbox installs.

--- full-iso/airootfs/root/customize_airootfs.sh
+++ full-iso/airootfs/root/customize_airootfs.sh
@@ -139,3 +139,7 @@ sed -i 's|local visible, action = cmd(item, self)|local visible, action = cmd(0,
 # lxdm
 rm -rf /etc/lxdm
 mv /etc/lxdm-blackarch /etx/lxdm
+
+# fluxbox
+rm -rf /usr/share/fluxbox
+cp -r /root/.fluxbox /usr/share/fluxbox
