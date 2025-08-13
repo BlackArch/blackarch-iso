081566cd6e36c4cd734936dbaaaaec9fb4a652a9
ikstream <stefan.venz@protonmail.com>
2025-08-13 21:09:33 +0200

    netinstall: move to upstream: some adjustement still missing
    Signed-off-by: ikstream <stefan.venz@protonmail.com>

--- netinstall-iso/profiledef.sh
+++ netinstall-iso/profiledef.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="blackarch-linux-netinst"
-iso_label="BLACKARCH_$(date +%Y%m)"
-iso_publisher="BlackArch Linux <https://www.blackarch.org/>"
-iso_application="BlackArch Linux NetInstall ISO"
-iso_version="$(date +%Y.%m.%d)"
-install_dir="blackarch"
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+iso_name="blackarchlinux"
+iso_label="BLACKARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="BlackArch Linux <https://blackarch.org>"
+iso_application="BlackArch Linux Live/Rescue DVD"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
+           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
-
